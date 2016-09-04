1f8c29b6aa8bb0d764937d658966ff07b3ddf154
Nate Reed <nate@natereed.com>
2016-09-04 08:55:30 -0500

    Got working prediction method. Returns the entry from vocab, including p, terms_counts, doc_counts. Those might be useful.

--- model/predict.R
+++ model/predict.R
@@ -1,51 +1,60 @@
 load_vocabs <- function() {
-  vocab = c()
-  vocab.append(readRDS(file.path("data", "model", "unigrams.RDS")))
-  vocab.append(readRDS(file.path("data", "model", "bigrams.RDS")))
-  vocab.append(readRDS(file.path("data", "model", "trigrams.RDS")))
-  vocab.append(readRDS(file.path("data", "model", "quadgrams.RDS")))
-  return(vocab)
+  vocabs <- list()
+  vocabs[[1]] <- readRDS(file.path("data", "model", "unigrams.RDS"))
+  vocabs[[2]] <- readRDS(file.path("data", "model", "bigrams.RDS"))
+  vocabs[[3]] <- readRDS(file.path("data", "model", "trigrams.RDS"))
+  vocabs[[4]] <- readRDS(file.path("data", "model", "quadgrams.RDS"))
+  return(vocabs)
 }
 
 get_regex <- function(input) {
   return(paste(paste("^", input, sep=""), "_", sep=""))
 }
 
-find_match <- function(ngram, vocab_dt) {
+find_match <- function(ngram, vocab) {
   pat <- get_regex(ngram)
-  matches <- vocab_dt[grep(pat, vocab_dt$terms),]
+  matches <- vocab[grep(pat, vocab$terms),]
   if (nrow(matches) > 0) {
-    return(matches[which.min(matches$p),])
+    return(matches[which.max(matches$p),])
   } else {
     return(NULL)
   }
 }
 
-count_n <- function(term) {
-  return(length(strsplit(term, '_')[[1]]))
+count_n <- function(ngram) {
+  return(length(strsplit(ngram, '_')[[1]]))
 }
 
 get_highest_ranking_term <- function() {
-  # Return most frequent unigram  
+  return(unigrams[which.max(unigrams$p)])  
 }
 
-# Given n-gram, get the n-1 n-gram up to the last word
-get_sub_ngram(ngram) {
-  
+# Given n-gram, the n-gram that starts with the second word
+get_sub_ngram <- function(ngram, n) {
+  return(paste(strsplit(ngram, '_')[[1]][2:n], collapse="_"))
+}
+
+slice_ngram <- function(ngram, start, end) {
+  return(paste(strsplit(ngram, '_')[[1]][start:end], collapse="_"))
 }
 
 # Take n-gram up to n=3 and try to predict the next term
 predict <- function(ngram) {
   n <- count_n(ngram)
-  match <- find_match(ngram, vocabs[n])
+  if (n > 3) {
+    ngram <- slice_ngram(ngram, n - 2, n)
+    n <- 3
+  }
+  
+  match <- find_match(ngram, vocabs[[n+1]])
   if (!is.null(match)) {
     return(match);
   } else {
     if (n == 1) {
       return(get_highest_ranking_term())
     }
-    return(predict(get_sub_ngram(ngram)));
+    return(predict(get_sub_ngram(ngram, n)));
   }
-  
 }
 
+vocabs <- load_vocabs();
